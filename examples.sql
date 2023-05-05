1480cdb5e273b7f462372bb617142ad74e4570a8
Zaahir Dawood <zaahirdawood@gmail.com>
2023-05-05 13:20:29 +0400

    Update examples.sql

--- examples.sql
+++ examples.sql
@@ -1,7 +1,6 @@
+-- Translating business questionsn to SQL Syntax
 
-
-#1. Count the user activation Rate
-
+--1. Count the user activation Rate
 -- Postgres 14 Syntax 
 
 WITH CTE AS (
@@ -16,3 +15,19 @@ WITH CTE AS (
       FROM CTE
     GROUP BY count_confirmed,count_not_confirmed
 
+--2. How well do credit cards typically do first month of launch (question derived by a companies need to launch a new credit card)
+    -- we are analysing the number of requested credit cards upon first month the cards were launched.
+WITH CTE AS (
+      SELECT 
+      card_name, 
+      issued_amount,
+      MAKE_DATE(issue_year,issue_month,1) as issue_date, 
+      MIN(MAKE_DATE(issue_year,issue_month,1)) OVER(PARTITION BY card_name) min_issue_date
+      FROM monthly_cards_issued
+      ORDER BY issued_amount DESC
+            )
+
+SELECT card_name, issued_amount
+FROM CTE
+WHERE issue_date = min_issue_date
+ORDER BY issued_amount DESC
